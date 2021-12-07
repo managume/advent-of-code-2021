43107a92972cc434b55435652fd6f4e47a46ce92
Manu Aguilar <manuel.aguilar@solucionex.com>
2021-12-07 11:35:56 +0100

    added day 6 first puzzle

---
+++ day6/puzzle01.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync('input.txt', 'utf-8').split(',').map((num) => parseInt(num))
+
+function latternfishSimulation(input,days) {
+    var state = Array.from(input);
+    for (let index = 1; index <= days; index++) {
+        state.map((num, index, array) => {
+            if( num == 0){
+                array[index] = 6
+                array.push(8)
+            }else{
+                array[index] -= 1
+            }
+        })
+    }
+    return state.length
+}
+
+var ret = latternfishSimulation(input,80);
+console.log(ret)
